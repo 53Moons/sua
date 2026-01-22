2157f6220dec7849a8d9da864250a3f4d0dccc6d
Blake Bradford <james.b.bradford@faa.gov>
2026-01-22 06:04:43 -0500

    Add UpdateDependantMilestonesOnDateChange

---
+++ plugins/SpecialUseAirspace/SUAPlugins/AeronauticalMilestone/UpdateDependantMilestonesOnDateChange.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xrm.Sdk;
+
+namespace SUAPlugins.AeronauticalMilestone
+{
+    public class UpdateDependantMilestonesOnDateChange : PluginBase
+    {
+        public UpdateDependantMilestonesOnDateChange()
+            : base(typeof(UpdateDependantMilestonesOnDateChange))
+        {
+            // not implemented
+        }
+
+        protected override void ExecuteCdsPlugin(ILocalPluginContext localPluginContext)
+        {
+            var context = localPluginContext.PluginExecutionContext;
+
+            if (context.MessageName != "Update" && context.MessageName != "Create")
+                return;
+
+            var sysService = localPluginContext.SystemUserService;
+            var tracer = localPluginContext.TracingService;
+
+            if (!context.InputParameters.TryGetValue("Target", out Entity target))
+                throw new InvalidPluginExecutionException("Target not found in context");
+
+            if (!context.PreEntityImages.TryGetValue("PreImage", out Entity preImage))
+                throw new InvalidPluginExecutionException("PreImage required on update");
+        }
+    }
+}
---
+++ plugins/SpecialUseAirspace/SUAPlugins/Utilities.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace SUAPlugins
+{
+    public static class Utilities
+    {
+        public static T GetValueOnUpdate<T>(Entity target, Entity preImage, string attribute)
+        {
+            if (target == null)
+                throw new ArgumentNullException("target");
+            if (preImage == null)
+                throw new ArgumentNullException("preImage");
+
+            if (!target.TryGetAttributeValue(attribute, out T value))
+            {
+                preImage.TryGetAttributeValue(attribute, out value);
+            }
+            return value;
+        }
+    }
+}
