51cddefbb66a5a45751dbdbaabeb392fd6a81813
Blake Bradford <james.b.bradford@faa.gov>
2026-01-23 18:34:43 -0500

    Add Utilities class with GetActionFromAeronauticalRef
    Introduced a static Utilities class in the SUAPlugins.Action namespace. Added the GetActionFromAeronauticalRef method to retrieve a related "sua_action" entity from an Aeronautical EntityReference using IOrganizationService, with error handling and tracing.

---
+++ plugins/SpecialUseAirspace/SUAPlugins/Action/Utilities.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Linq;
+
+namespace SUAPlugins.Action
+{
+    public static class Utilities
+    {
+        public static Entity GetActionFromAeronauticalRef(
+            EntityReference aeroRef,
+            IOrganizationService service,
+            ITracingService tracer
+        )
+        {
+            if (aeroRef == null)
+                throw new ArgumentNullException("aeroRef");
+            if (service == null)
+                throw new ArgumentNullException("service");
+            if (tracer == null)
+                throw new ArgumentNullException("tracer");
+
+            Entity action;
+            try
+            {
+                var actionQuery = new QueryExpression("sua_action")
+                {
+                    ColumnSet = new ColumnSet(true),
+                    Criteria = new FilterExpression
+                    {
+                        Conditions =
+                        {
+                            new ConditionExpression(
+                                "sua_aeronautical",
+                                ConditionOperator.Equal,
+                                aeroRef.Id
+                            )
+                        }
+                    }
+                };
+
+                action =
+                    service.RetrieveMultiple(actionQuery).Entities.FirstOrDefault()
+                    ?? throw new InvalidPluginExecutionException("Related Action not found");
+
+                tracer.Trace($"Related Action: {action.Id}");
+            }
+            catch (Exception ex)
+            {
+                tracer.Trace($"Error retrieving Action from Aeronautical: {ex.Message}");
+                throw;
+            }
+
+            return action;
+        }
+    }
+}
