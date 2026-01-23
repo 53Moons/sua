fef3c200debaf95803cefaa0c1a005b838e74581
Blake Bradford <james.b.bradford@faa.gov>
2026-01-23 17:07:23 -0500

    Add PreventAutoSave function to manage auto-save behavior in Xrm events

--- src/General/index.ts
+++ src/General/index.ts
@@ -1,3 +1,13 @@
 export * from './Attributes';
 export * from './Tabs';
 export * from './Types';
+
+export function PreventAutoSave(context: Xrm.Events.SaveEventContext): void {
+    if (!context) return;
+    const eventArgs = context.getEventArgs();
+    const saveMode = eventArgs.getSaveMode();
+    if (saveMode == 70) {
+        eventArgs.preventDefault();
+        console.debug('PreventAutoSave: Save operation prevented for saveMode 70 (AutoSave)');
+    }
+}
